76d138ac30641f5a9aefc2adf088807443665dd9
KalaiarasiPattusamy <p.kalaiarasi1999@gmail.com>
2021-07-27 22:40:15 +0530

    day7 assignments

---
+++ day7assignment-27july/27july-kalaiarasi-day7/forlooptask.py
@@ -0,0 +1,20 @@
+#l=[2,6,3,9,5]
+#for x in l:
+#   if x%2==0:
+#      print(x)
+#for x in "india"
+#  print(x)
+
+#count program                         
+
+#st="India is my country"
+#count=0
+#for x in st:
+#  if(x=='a' or x=='e' or x=='i' or x=='o' or x=='u' or x=='A' or x=='E' or x=='I' or x=='O' or x=='U'):
+#    count=count+1
+#print(count)
+
+for i in range(11):
+  print(i,end=" ")
+#for i in range(1,10,2):
+#  print(i)                
---
+++ day7assignment-27july/27july-kalaiarasi-day7/whileass.py
@@ -0,0 +1,10 @@
+#i=1
+#while(i<=10):
+  #  print(i)
+  #  i=i+1
+
+i=int(input("enter start:"))
+endvalue=int(input("enter end:"))
+while(i<=endvalue):
+    print(i,end=" ")
+    i=i+1
